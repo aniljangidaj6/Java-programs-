8ec2bfac5d0841a4dab245e007ba95912b706825
root <root@localhost.localdomain>
2017-08-16 00:26:16 +0530

    Write string to a file

---
+++ writedata.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+class writedata
+{
+  public static void main(String args[])  throws IOException
+  {
+    File file = new File("myfile.txt");
+
+    file.createNewFile(); //creates New File
+
+FileWriter w = new FileWriter(file);
+
+w.write("hello world");
+w.flush();
+w.close();
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
